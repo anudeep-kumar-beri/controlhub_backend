30378d0486d93d255012231f69f445f9be7fbf68
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-06 16:51:29 +0530

    Update bookmark.js

--- models/bookmark.js
+++ models/bookmark.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const BookmarkSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  url: { type: String, required: true },
+  link: { type: String, required: true },
   category: { type: String }
 }, { timestamps: true });
 
