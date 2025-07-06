bc519d41ff394b8367906d818bb1cae950d8eb2c
anudeep-kumar-beri <anudeepkumarberi@gmail.com>
2025-07-06 12:08:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://controlhub-frontend-cbegxqxtu-anudeep-kumar-beris-projects.vercel.app',
+  credentials: true // Only if you use cookies or authentication headers
+}));
 app.use(express.json());
 
 // Import and Mount routes
